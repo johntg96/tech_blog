3c2f84ce652429f7c5a1ca40013f9fbce0c41e41
John Garrison <splendidonkey@gmail.com>
2023-08-26 11:29:18 -0500

    fix config connection file for heroku

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    // .env file is working for server.js but not for seeding.
-    // ... so.. for now I am just placing the localhost database credentials here.
-  //  process.env.DB_NAME,
-  'tech_blog_db',
-  //  process.env.DB_USER,
-  'root',
-  //  process.env.DB_PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   null,
     {
       host: 'localhost',
