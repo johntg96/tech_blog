0ccc1fc379ca5a0dc9599bf0ae141da49ae506ed
John Garrison <splendidonkey@gmail.com>
2023-08-26 11:57:52 -0500

    minor fixes

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,12 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  'tech_blog_db',
+  // process.env.DB_USER,
+  'root',
+  // process.env.DB_PASSWORD,
+  null,
     {
       host: 'localhost',
       dialect: 'mysql',
